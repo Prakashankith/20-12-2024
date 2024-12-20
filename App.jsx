a6447c174855df38bdfe56a62f65192894ed9a8c
ANKITH B.P <sirankithbp1999@gmail.com>
2024-12-20 19:51:50 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+import Counter from './Counter';
+import Dumbo from './Dumbo'
+import ScoreKeeper from './Scorekeeper';
+import EmojiClicker from './EmojiClicker';
+
+
+function App() {
+
+  return (
+    <div>
+      {/* <h1>hii</h1> */}
+      {/* <Counter /> */}
+
+      {/* <Dumbo /> */}
+      {/* <ScoreKeeper /> */}
+
+      <EmojiClicker />
+
+    </div>
+
+
+  )
+}
+
+export default App;
